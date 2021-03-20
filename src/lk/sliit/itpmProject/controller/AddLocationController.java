26789259dad01ad9cab62d60916b75b6a8f4321f
rivinduchamath <trivinduchamath333@gmail.com>
2021-03-20 17:03:38 +0530

    Add Navigate

--- src/lk/sliit/itpmProject/controller/AddLocationController.java
+++ src/lk/sliit/itpmProject/controller/AddLocationController.java
@@ -1,4 +1,83 @@
 package lk.sliit.itpmProject.controller;
 
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 public class AddLocationController {
+
+    @FXML
+    private AnchorPane root1;
+
+    @FXML
+    private ImageView iconHome;
+
+    @FXML
+    private ImageView iconLecture;
+
+    @FXML
+    private ImageView iconStudent;
+
+    @FXML
+    private ImageView iconTimeTable;
+
+    @FXML
+    void navigate(MouseEvent event) throws IOException {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            Parent root = null;
+
+            FXMLLoader fxmlLoader = null;
+            switch (icon.getId()) {
+                case "iconHome":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/MainForm.fxml"));
+                    break;
+                case "iconStudent":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/AddStudent.fxml"));
+                    break;
+                case "iconLecture":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/AddLecturer.fxml"));
+                    break;
+                case "iconTimeTable":
+                    fxmlLoader = new FXMLLoader(this.getClass().getResource("../view/AddWorkingDaysAndHours.fxml"));
+                    root = fxmlLoader.load();
+                    break;
+            }
+
+            if (root != null) {
+                Scene subScene = new Scene(root);
+                Stage primaryStage = (Stage) this.root1.getScene().getWindow();
+
+                primaryStage.setScene(subScene);
+                primaryStage.centerOnScreen();
+
+                TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+                tt.setFromX(-subScene.getWidth());
+                tt.setToX(0);
+                tt.play();
+
+            }
+        }
+    }
+
+    @FXML
+    void playMouseEnterAnimation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseExitAnimatio(MouseEvent event) {
+
+    }
+
 }
