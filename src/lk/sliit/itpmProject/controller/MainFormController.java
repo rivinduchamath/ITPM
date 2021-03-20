3828627ebdddfe7b8286af52c1bd25c423d819d8
rivinduchamath <trivinduchamath333@gmail.com>
2021-03-20 14:45:53 +0530

    Add Navigate

--- src/lk/sliit/itpmProject/controller/MainFormController.java
+++ src/lk/sliit/itpmProject/controller/MainFormController.java
@@ -1,4 +1,148 @@
 package lk.sliit.itpmProject.controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MainFormController {
+
+    @FXML
+    private ImageView iconStudent;
+
+    @FXML
+    private ImageView iconMap;
+
+    @FXML
+    private ImageView iconLecture;
+
+    @FXML
+    private ImageView iconTimeTable;
+
+    @FXML
+    private Label lblMenu;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label pgb;
+    @FXML
+    private AnchorPane root;
+
+
+    public void initialize(URL url, ResourceBundle rb) {
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(2000), root);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+    @FXML
+    private void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            icon.setEffect(null);
+            lblMenu.setText("Welcome");
+            lblDescription.setText("Please select one of above main operations to proceed");
+        }
+    }
+
+    @FXML
+    private void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            switch (icon.getId()) {
+                case "iconStudent":
+                    lblMenu.setText("Manage Student");
+                    lblDescription.setText("Click to add, edit, delete, search Student");
+                    break;
+                case "iconMap":
+                    lblMenu.setText("Find Location");
+                    lblDescription.setText("Click to search Locations");
+                    break;
+                case "iconLecture":
+                    lblMenu.setText("Manage Lecture");
+                    lblDescription.setText("Click here to Manage Lecturers");
+                    break;
+                case "iconTimeTable":
+                    lblMenu.setText("Manage Time Table");
+                    lblDescription.setText("Click if you want to search Time Tables");
+                    break;
+            }
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.GREEN);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
+    }
+
+    @FXML
+    private void navigate(MouseEvent event) throws IOException {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            Parent root = null;
+
+            FXMLLoader fxmlLoader = null;
+            switch (icon.getId()) {
+                case "iconStudent":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/AddStudent.fxml"));
+                    break;
+                case "iconMap":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/AddLocation.fxml"));
+                    break;
+                case "iconLecture":
+                    root = FXMLLoader.load(this.getClass().getResource("../view/AddLecturer.fxml"));
+                    break;
+                case "iconTimeTable":
+                    fxmlLoader = new FXMLLoader(this.getClass().getResource("../view/AddWorkingDaysAndHours.fxml"));
+                    root = fxmlLoader.load();
+                    break;
+            }
+
+            if (root != null) {
+                Scene subScene = new Scene(root);
+                Stage primaryStage = (Stage) this.root.getScene().getWindow();
+
+                primaryStage.setScene(subScene);
+                primaryStage.centerOnScreen();
+
+                TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+                tt.setFromX(-subScene.getWidth());
+                tt.setToX(0);
+                tt.play();
+
+            }
+        }
+    }
+
 }
