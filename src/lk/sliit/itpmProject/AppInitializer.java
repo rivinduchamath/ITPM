5c03bef474d80d3933418fb7d37c156e0d544671
rivinduchamath <trivinduchamath333@gmail.com>
2021-04-02 14:08:25 +0530

    Slove error

--- src/lk/sliit/itpmProject/AppInitializer.java
+++ src/lk/sliit/itpmProject/AppInitializer.java
@@ -15,8 +15,7 @@ import java.util.logging.*;
 
 public class AppInitializer extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) { launch(args);
     }
 
     @Override
